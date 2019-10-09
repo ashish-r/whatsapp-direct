3653e0d6999a5c8ae6dcd464e6ad64259cd5fe34
Ashish Ranjan <ashish.ranjan@coverfoxmail.com>
2019-10-10 00:11:01 +0530

    sw new events

--- sw.js
+++ sw.js
@@ -1,13 +1,12 @@
-var cacheName = 'whatsapp-direct'
-var filesToCache = [
-    'index.html',
-    'css/style.css',
-    'js/sw-install.js',
-    'js/index.js',
-]
-
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
+window.addEventListener('install', function(e) {
+    const cacheName = 'whatsapp-direct'
+    const filesToCache = [
+        'index.html',
+        'css/style.css',
+        'js/sw-install.js',
+        'js/index.js',
+    ]
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
             return cache.addAll(filesToCache)
@@ -16,10 +15,23 @@ self.addEventListener('install', function(e) {
 })
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+window.addEventListener('fetch', function(e) {
     e.respondWith(
         caches.match(e.request).then(function(response) {
             return response || fetch(e.request)
         })
     )
+})
+
+/* Trigger when app install successful */
+window.addEventListener('appinstalled', (evt) => {
+    document.getElementById("mobile_number").value = 9
+})
+
+let deferredPrompt
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e
+    document.getElementById("mobile_number").value = 999
 })
