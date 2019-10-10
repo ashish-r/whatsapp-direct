ef67eb463a6c716912b29d114546369bdf1015bd
Ashish Ranjan <ashish.ranjan@coverfoxmail.com>
2019-10-10 12:05:56 +0530

    sw

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const VERSION = '-v10'
-const CACHE_NAME = 'whatsapp-direct' + VERSION
+const FILE_VERSION = '-v10'
+const CACHE_NAME = 'whatsapp-direct' + FILE_VERSION
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
     const filesToCache = [
@@ -12,7 +12,7 @@ self.addEventListener('install', function(e) {
         caches.open(CACHE_NAME)
         .then(function(cache) {
             return cache.addAll(filesToCache.map(function(file){
-                file + VERSION
+                file + FILE_VERSION
             }))
         })
         .then(function() {
