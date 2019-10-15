ae901d92992539c020adfe2702fdd515be58b495
Ashish Ranjan <ashish.ranjan@coverfoxmail.com>
2019-10-15 17:41:46 +0530

    test toast

--- scripts/sw.ts
+++ scripts/sw.ts
@@ -1,5 +1,5 @@
 import { ServiceWorkerEvent, ServiceWorkerGlobalContext } from "./types"
-const FILE_VERSION = '?v201'
+const FILE_VERSION = '?v202'
 const CACHE_NAME = 'whatsapp-direct' + FILE_VERSION
 
 /* Start the service worker and cache all of the app's content */
